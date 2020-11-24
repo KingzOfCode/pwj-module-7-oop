c0682881ccbea347ca16d8bc8405bfb90997179d
Shayon <shayon@comcast.net>
2020-11-24 15:57:48 -0500

    completed

--- exercises/beatBoxJS/script.js
+++ exercises/beatBoxJS/script.js
@@ -1,47 +1,56 @@
 /**
- * TODO: Create a keydown listener to track what keys are hit
- * TODO: Create a Beat class to represent the beat object in utils.js
- * TODO: Complete triggerBeat() to play upon the press of a,s,d,f,g,h,j,k,l
- * TODO: Button pt1: Initialize color and element values
- * TODO: Button pt2: Set button color upon initialization | Initialize button in beats["65"]
- * TODO: Button pt3: Complete select function to set the color and shadow of button upon pressing
- * TODO: Button pt4: Call the select() function upon key press ;)
- * TODO: Button pt5: Add transition for button selection
- * TODO: Button pt6: Remove the button style upon transition end | Use deselect function
- * TODO: Complete all button instances with the following colors
- * TODO: Add background image
+ * Done: Create a keydown listener to track what keys are hit
+ * Done: Create a Beat class to represent the beat object in utils.js
+ * Done: Complete triggerBeat() to play upon the press of a,s,d,f,g,h,j,k,l
+ * Done: Button pt1: Initialize color and element values
+ * Done: Button pt2: Set button color upon initialization | Initialize button in beats["65"]
+ * Done: Button pt3: Complete select function to set the color and shadow of button upon pressing
+ * Done: Button pt4: Call the select() function upon key press ;)
+ * Done: Button pt5: Add transition for button selection
+ * Done: Button pt6: Remove the button style upon transition end | Use deselect function
+ * Done: Complete all button instances with the following colors
+ * Done: Add background image
  * First 3: #00fffe
  * 4,5,6,7: #FF00FF
  * 8, 9: #FFFFFF
  */
 
 let beats = {
-    "65": {
+    "103": {
         beat: new Beat("./assets/Piano Chord 331.mp3"),
+        button: new Button('#00fffe', 65)
     },
-    "83": {
+    "104": {
         beat: new Beat("./assets/Piano Chord 209.mp3"),
+        button: new Button('#00fffe', 83)
     },
-    "68": {
+    "105": {
         beat: new Beat("./assets/Piano Chord 208.mp3"),
+        button: new Button('#00fffe', 68)
     },
-    "70": {
+    "100": {
         beat: new Beat("./assets/Drum Sticks Hit 3.mp3"),
+        button: new Button('#FF00FF', 70)
     },
-    "71": {
+    "101": {
         beat: new Beat("./assets/Drum Snare Roll.mp3"),
+        button: new Button('#FF00FF', 71)
     },
-    "72": {
+    "102": {
         beat: new Beat("./assets/PREL Musical 57.mp3"),
+        button: new Button('#FF00FF', 72)
     },
-    "74": {
+    "97": {
         beat: new Beat("./assets/Cymbal Suspended 2.mp3"),
+        button: new Button('#FF00FF', 74)
     },
-    "75": {
+    "98": {
         beat: new Beat("./assets/Musical Compos 33.mp3"),
+        button: new Button('#FFFFFF', 75)
     },
-    "76": {
+    "99": {
         beat: new Beat("./assets/Musical Orches 4.mp3"),
+        button: new Button('#FFFFFF', 76)
     }
 }
 
@@ -50,10 +59,42 @@ let beats = {
  * HINT: use the keyCode
  */
 triggerBeat = (event) => {
-
+    const keyCode = event.keyCode;
+    if(keyCode in beats){
+        let keyPress = beats[keyCode];
+        keyPress.beat.play();
+        //console.log('it\'s working');
+    if(keyCode in beats){
+        let keyPress = beats[keyCode];
+        keyPress.button.select();
+        //console.log('it\'s working')
+    }
+    }
+    document.getElementById('65').addEventListener('keydown', 103);
+    console.log(event);
 }
 
 /**
  * Keydown listener to fire triggerBeat function
  * HINT: Log the keyCode of the key
  */
+
+document.addEventListener('keydown', triggerBeat) /*=> {
+    console.log(event);
+    console.log(event.keyCode);
+    triggerBeat
+}*/
+
+
+// window.onload = function() {
+//     document.getElementById('65').addEventListener('103', beat);
+//     domEvents();
+// }
+// input.addEventListener("keydown", function(event) {
+//     if (event.keyCode === 103) {
+//       // Cancel the default action, if needed
+//       event.preventDefault();
+//       // Trigger the button element with a click
+//       document.getElementById("65").click();
+//     }
+//   });
--- exercises/beatBoxJS/utils.js
+++ exercises/beatBoxJS/utils.js
@@ -3,7 +3,19 @@
  * HINT: Make sure to pass in the audioSrc as parameter to create a new audio track
  * HINT: Create a play function to play the audio if called 
  */
+
+//  let test = new Audio('');
+//  test.play();
 class Beat {
+        constructor(audioSrc){
+            this.audio = new Audio(audioSrc);
+            //console.log(this.audio);
+            //this.audio.play();
+        }
+        play = () => {
+            this.audio.currentTime = 0; //reset the audio file
+            this.audio.play()
+        }
 }
 
 
@@ -13,23 +25,48 @@ class Beat {
  */
 class Button {
     constructor(color, keyCode){
+        this.color = color;
+        this.keyCode = keyCode;
+        this.element = document.getElementById(keyCode);
+        this.setButtonColorInHTML();
+        this.setATransitionEndListener();
+        //this.select();
+        //console.log(this.element);
     }
+
+    /*
+    Solution 1: remove style on keyup
+    Solution 2: wait a 0.2s to remove style 
+    Solution 3: react on transition end event
+    */
+   setATransitionEndListener = () => {
+       this.element.addEventListener('transitionend', () => {
+           this.deselect();
+           //console.log('transition ended');
+       })
+   }
 
     /**
      * Set the button color based on color specified
      */
     setButtonColorInHTML = () => {
+        this.element.style.borderColor = this.color;
     }
 
     /**
      * Select function to set the background color and boxShadow
+     * 0px 0px 17px 0px #FFFFFF
      */
     select = () => {
+        this.element.style.backgroundColor = this.color;
+        this.element.style.boxShadow = `0px, 0px, 17px, 0px, #this.color`;
     }
 
     /**
      * Deselect function to reset background color and boxShadow
      */
     deselect = () => {
+        this.element.style.backgroundColor = 'transparent';
+        this.element.style.boxShadow = 'none';
     }
 }
